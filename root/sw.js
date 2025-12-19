00b9321f3aea18c87fc75ace285724b88121038c
Mayuk <mayuksen72@gmail.com>
2025-12-19 15:24:06 +0530

    Update sw.js

--- root/sw.js
+++ root/sw.js
@@ -12,3 +12,10 @@ self.options = {
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
 
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10347241
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
